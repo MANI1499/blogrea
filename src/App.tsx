8179519f07801c458ce3336889b2078f5a8876ba
MANI1499 <nanaacheampong30@gmail.com>
2023-07-04 22:20:27 +0000

    type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Person, Country } from './components/Person'
 
 function App() {
   return (
     <div className="App">
-      
+      <Person 
+        name="Mani"
+        email = "nana@mail.com"
+        age = {23}
+        isMarried = {true}
+        friends = {[ 'jake', 'mani', 'jessica']}
+        country = { Country.Haiti}
+      />
+
     </div>
   );
 }
